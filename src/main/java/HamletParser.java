95bff878b250edfefae58add97f37dad2e1e260b
taylor <taylormyers01@comcast.net>
2023-08-15 14:39:58 -0400

    Updated functionality to replace hamlet

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -17,18 +19,15 @@ public class HamletParser {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
-
         try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+            while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
-
             scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
-
         return result.toString();
     }
 
@@ -36,4 +35,64 @@ public class HamletParser {
         return hamletData;
     }
 
+
+    public String changeHamletToLeon(String input){
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll("Leon");
+    }
+    public String changeHoratioToTariq(String input){
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll("Tariq");
+    }
+
+    public int findHamlet(String input){
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+        int numOfOccurrence = 0;
+        while(matcher.find()){
+            numOfOccurrence++;
+        }
+        return numOfOccurrence;
+    }
+
+    public int findHoratio(String input){
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+        int numOfOccurrence = 0;
+        while(matcher.find()){
+            numOfOccurrence++;
+        }
+        return numOfOccurrence;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -15,17 +15,43 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        HamletParser hp = new HamletParser();
+        String input = "Hamlet was Ham of Letting with Hamlet as humlets Hams";
+        String expected = "Leon was Ham of Letting with Leon as humlets Hams";
+
+        String actual = hp.changeHamletToLeon(input);
+
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        HamletParser hp = new HamletParser();
+        String input = "Horatio is the best at gettinghoratio Ratiod with Horatio himself Harotio ";
+        String expected = "Tariq is the best at gettingTariq Ratiod with Tariq himself Harotio ";
+        String actual = hp.changeHoratioToTariq(input);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        HamletParser hp = new HamletParser();
+        String input = "Horatio is the best at gettinghoratio Ratiod with Horatio himself Harotio ";
+        int expected = 3;
+        int actual = hp.findHoratio(input);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        HamletParser hp = new HamletParser();
+        String input = "Hamlet was Ham of Letting with Hamlet as humlets Hams";
+        int expected = 2;
+        int actual = hp.findHamlet(input);
+
+        assertEquals(expected, actual);
     }
 }
